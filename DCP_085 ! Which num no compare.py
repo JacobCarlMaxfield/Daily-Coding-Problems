2635a357adfca0356514b05a2e8356916a0f710c
MaxTechniche <Max.Techniche@gmail.com>
2020-10-24 20:06:56 -0600

    Uncomplete

---
+++ DCP_085 ! Which num no compare.py
@@ -0,0 +1,15 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Facebook.
+
+Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, using only mathematical or bit operations. You can assume b can only be 1 or 0.
+"""
+
+
+def which_num(b, x, y):
+    pass
+
+
+print(which_num(1, 5, 8))
+print(which_num(0, 3, 4))
